c022d6a1fb04b10ac077e3dcb7d01f4254f37e96
andreewaD <51123966+andreewD@users.noreply.github.com>
2021-10-06 15:56:39 -0500

    Alert modal fixed

--- src/components/AlertModalForm/AlertModalForm.js
+++ src/components/AlertModalForm/AlertModalForm.js
@@ -6,11 +6,12 @@ export default function AlertModalForm(props) {
             <div className='Alet-modal-background'>
                 <div className='Alert-modal'>
                     <div className='Box-Modal'>
-                    <div className='Button-Modal'>
-                            <button onClick={props.closeModal}>cerrar</button>
-                        </div>
+                    {/* <div className='Button-Modal'>
+                            <button onClick={props.closeModal}>X</button>
+                        </div> */}
                         <div className='title-Modal'>
-                            <h1>¡HOLA!</h1>
+                            <h1 className="text">¡HOLA!</h1>
+                            <button className="button" onClick={props.closeModal}>X</button>
                         </div>
                         <div className='paragraph-Modal'>
                             <p>Gracias por tu interés en Dr.tesis</p>
