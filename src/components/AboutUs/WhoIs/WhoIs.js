ed40cb36950c9d7ee671aec545102390f452189c
andreewaD <51123966+andreewD@users.noreply.github.com>
2021-10-05 20:01:40 -0500

    AboutUs + Services

--- src/components/AboutUs/WhoIs/WhoIs.js
+++ src/components/AboutUs/WhoIs/WhoIs.js
@@ -1,10 +1,34 @@
 import React from 'react'
-
+import './WhoIs.scss';
+import People from '../../../assets/imgs/AboutUs/people.png'
 export default function WhoIs() {
-    
+
     return (
-        <div>
-            
+
+        <div className="whoIsContainer">
+            <div className="leftContainer">
+                <div className="firstTitle">
+                    ACERCA DE NOSOTROS
+                </div>
+                <div className="secondTitle">
+                    ¿Quién es<br/> Dr. Tesis?
+                </div>
+                <div className="content">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam no
+                    nummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+                    volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+                    ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+                    consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate
+                    velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis
+                    at vero eros et accum
+                </div>
+                <button className="buttonBlack"><h1>Solicita tu cotización</h1></button>
+            </div>
+            <div className="rightContainer">
+                <img className="imgRight" src={People} alt="" />
+            </div>
         </div>
+
+
     )
 }
--- src/pages/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import Abouts from "../../components/AboutUs/Valores/AboutUs"
 import AboutUsData from "../../components/AboutUs/Valores/AboutUsData.json"
+import WhoIs from '../../components/AboutUs/WhoIs/WhoIs'
 export default function AboutUs() {
     return (
-    <Abouts data={AboutUsData.aboutUs}/>
+        <div> 
+            <WhoIs />
+            <Abouts data={AboutUsData.aboutUs} />
+        </div>
+
     )
 }
